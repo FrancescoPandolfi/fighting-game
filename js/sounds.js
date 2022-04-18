ac8a755b3fbff894f126a2c9e3c670c092abf647
Francesco Pandolfi <francesco.pan@gmail.com>
2022-04-18 16:13:55 +0200

    sound volume

--- js/sounds.js
+++ js/sounds.js
@@ -1,14 +1,18 @@
 const sfx = {
     playerSword: new Howl({
+        volume: 0.5,
         src: './audio/mixkit-metal-hit-woosh-1485.wav'
     }),
     playerVoice: new Howl({
+        volume: 0.5,
         src: './audio/mixkit-effort-man-voice-2170.wav'
     }),
     enemyVoice: new Howl({
+        volume: 0.5,
         src: './audio/mixkit-fighting-man-voice-2172.wav'
     }),
     enemySword: new Howl({
+        volume: 0.5,
         src: './audio/mixkit-fantasy-sword-slide-2798.wav'
     }),
     bgMusic: new Howl({
